5177119c706d78006189a4a75e221083ec0a4bc6
taha berk <taha.kuyruk@spippaaaipay.com.tr>
2024-08-08 16:51:33 +0300

    More tests to tkinter

--- tkinterdeneme/tkinter_deneme.py
+++ tkinterdeneme/tkinter_deneme.py
@@ -2,8 +2,10 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-frm = ttk.Frame(root, padding = 10)
+frm = ttk.Frame( padding = 10)
 frm.grid()
 ttk.Label(frm, text = "Hello World!").grid(column = 0, row = 0)
 ttk.Button(frm, text = "Quit", command = root.destroy).grid(column = 1, row = 0)
+
+root.title('DENEME')
 root.mainloop()
